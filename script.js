632593b760306e42850fee90f96efee4660c3d0a
jetti16acksno <126006864+jetti16acksno@users.noreply.github.com>
2023-04-03 21:49:26 +0530

    Add files via upload
    First

---
+++ script.js
@@ -0,0 +1,38 @@
+// Get Popup Box
+var popup = document.getElementById('popup-box');
+
+// Get Traveller Popup Box
+var travelerPopup = document.getElementById('traveller-popup-box');
+
+// Get New Citizen Popup Box
+var newCitizenPopup = document.getElementById('new-citizen-popup-box');
+
+// Show Popup Box
+function showPopup() {
+popup.style.display = "block";
+}
+
+// Hide Popup Box
+function hidePopup() {
+popup.style.display = "none";
+}
+
+// Show Traveller Popup Box
+function showTravelerPopup() {
+travelerPopup.style.display = "block";
+}
+
+// Hide Traveller Popup Box
+function hideTravelerPopup() {
+travelerPopup.style.display = "none";
+}
+
+// Show New Citizen Popup Box
+function showNewCitizenPopup() {
+newCitizenPopup.style.display = "block";
+}
+
+// Hide New Citizen Popup Box
+function hideNewCitizenPopup() {
+newCitizenPopup.style.display = "none";
+}
